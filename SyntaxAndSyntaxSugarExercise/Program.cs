3167f60d2e1c75316d3da23b03a4b11d763e5b39
Derrick <Avirajoesph@yahoo.com>
2024-06-05 15:07:26 -0500

    fixxed project

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            var answer = 4;
+            string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to 9";
+            Console.WriteLine(response);
+              
+            }
         }
     }
-}
+
